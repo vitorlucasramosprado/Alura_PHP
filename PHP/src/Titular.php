8ff4f3345ec77db02c42eb1ad194405e03c4e31e
vitor <vitorlucasedif@gmail.com>
2022-03-16 17:26:37 -0300

    conhecendo e utilizando o pilar Herança na POO

--- PHP/src/Titular.php
+++ PHP/src/Titular.php
@@ -1,32 +1,18 @@
 <?php
 
-class Titular
+class Titular extends Pessoa
 {
-    private $cpf;
-    private $nome;
+    private Endereco $endereco;
 
-    public function __construct(CPF $cpf, string $nome)
+    public function __construct(CPF $cpf, string $nome, Endereco $endereco)
     {
-        $this->cpf = $cpf;
-        $this->validaNomeTitular($nome);
-        $this->nome = $nome;
+        parent::__construct($nome, $cpf);
+        $this->endereco = $endereco;    
     }
 
-    public function recuperaCpf(): string
-    {
-        return $this->cpf->recuperaNumero();
-    }
 
-    public function recuperaNome(): string
+    public function recuperaEndereco(): Endereco
     {
-        return $this->nome;
-    }
-
-    private function validaNomeTitular(string $nomeTitular)
-    {
-        if (strlen($nomeTitular) < 5) {
-            echo "Nome precisa ter pelo menos 5 caracteres";
-            exit();
-        }
+        return $this->endereco;
     }
 }
